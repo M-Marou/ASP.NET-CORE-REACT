40a83cad1b9b709f5d12db410acbb5027fb972e5
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2021-04-19 11:37:39 +0000

    Update Category.js

--- intelviastore-client/src/components/Category.js
+++ intelviastore-client/src/components/Category.js
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from 'react'
 const defaultImageSrc = '/img/categoryicon.png'
 
 const initialFieldValues = {
-    CategoryId: 0,
-    CategoryName:'',
-    ImageName: '',
-    ImageSrc: defaultImageSrc,
-    ImageFile: null
+    categoryID: 0,
+    categoryName:'',
+    imageName: '',
+    imageSrc: defaultImageSrc,
+    imageFile: null
 }
 
 export default function Category(props){
@@ -27,22 +27,22 @@ const handleInputChange = e => {
 
 const showPreview = e =>{
     if(e.target.files && e.target.files[0]){
-        let ImageFile = e.target.files[0];
+        let imageFile = e.target.files[0];
         const reader = new FileReader();
         reader.onload = x=>{
             setValues({
                 ...values,
-                ImageFile,
-                ImageSrc: x.target.result
+                imageFile,
+                imageSrc: x.target.result
             })
         }
-        reader.readAsDataURL(ImageFile)
+        reader.readAsDataURL(imageFile)
     }
     else{
         setValues({
             ...values,
-            ImageFile:null,
-            ImageSrc: defaultImageSrc
+            imageFile:null,
+            imageSrc: defaultImageSrc
         })
     }
 }
@@ -50,8 +50,8 @@ const showPreview = e =>{
 
 const Validate =()=>{
     let temp= {}
-    temp.CategoryName = values.CategoryName==""?false:true;
-    temp.ImageSrc = values.ImageSrc==defaultImageSrc?false:true;
+    temp.categoryName = values.categoryName==""?false:true;
+    temp.imageSrc = values.imageSrc==defaultImageSrc?false:true;
     setErrors(temp)
     return Object.values(temp).every(x => x==true)
 }
@@ -68,10 +68,10 @@ const handleFormSubmit = e =>{
     if(Validate()){
 
         const formData = new FormData()
-        formData.append('CategoryId', values.CategoryId)
-        formData.append('CategoryName', values.CategoryName)
-        formData.append('ImageName', values.ImageName)
-        formData.append('ImageFile', values.ImageFile)
+        formData.append('categoryID', values.categoryID)
+        formData.append('categoryName', values.categoryName)
+        formData.append('imageName', values.imageName)
+        formData.append('imageFile', values.imageFile)
         addOrEdit(formData, resetForm)
        
     }
@@ -87,15 +87,15 @@ const applyErrorClass = field => ((field in errors && errors[field]==false)?' in
         </div>
         <form autoComplete="off" noValidate onSubmit={handleFormSubmit}>
             <div className="card">
-                <img src={values.ImageSrc} className="card-img-top" />
+                <img src={values.imageSrc} className="card-img-top" />
                 <div className="card-body">
                     <div className="form-group">
-                        <input type="file" accept="image/*" className={"form-control-file"+applyErrorClass('ImageSrc')}
+                        <input type="file" accept="image/*" className={"form-control-file"+applyErrorClass('imageSrc')}
                         onChange={showPreview} id="image-uploader" />
                     </div>
                     <div className="form-group">
-                        <input className={"form-control"+applyErrorClass('CategoryName')} placeholder="Category Name" name="CategoryName"
-                        value={values.CategoryName} 
+                        <input className={"form-control"+applyErrorClass('categoryName')} placeholder="category Name" name="categoryName"
+                        value={values.categoryName} 
                         onChange = {handleInputChange}/>
                     </div>
                     <div className="form-group text-center">
