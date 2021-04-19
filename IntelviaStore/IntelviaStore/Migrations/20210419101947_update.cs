7f299523e56f741bdfbc099d006f5e59cb226066
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2021-04-19 11:40:27 +0000

    Create 20210419101947_update.cs

---
+++ IntelviaStore/IntelviaStore/Migrations/20210419101947_update.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IntelviaStore.Migrations
+{
+    public partial class update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoriesId",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoriesId",
+                table: "Products",
+                newName: "CategoriesCategoryID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CategoriesId",
+                table: "Products",
+                newName: "IX_Products_CategoriesCategoryID");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Categories",
+                newName: "CategoryName");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "CategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoriesCategoryID",
+                table: "Products",
+                column: "CategoriesCategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoriesCategoryID",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoriesCategoryID",
+                table: "Products",
+                newName: "CategoriesId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CategoriesCategoryID",
+                table: "Products",
+                newName: "IX_Products_CategoriesId");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryName",
+                table: "Categories",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryID",
+                table: "Categories",
+                newName: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoriesId",
+                table: "Products",
+                column: "CategoriesId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
